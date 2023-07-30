d830f54b890c7397cc54b7ff9cb13bde3e9038a5
skitou <chiantigally@gmail.com>
2023-07-30 04:02:51 +0200

    - Fixed config file location with the saveModifications method - Added sentry and logging to exceptions
    Signed-off-by: skitou <chiantigally@gmail.com>

--- src/main/java/fr/skitou/botcore/core/Config.java
+++ src/main/java/fr/skitou/botcore/core/Config.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import io.sentry.Sentry;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -23,6 +24,9 @@ public enum Config {
     private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
+    private final String configFileName = "config.json";
+    private final File configFile = new File("data", configFileName);
+
     @Getter
     private JsonObject rootJson, defaultJson;
 
@@ -30,8 +34,7 @@ public enum Config {
      * Initiate configuration (parse JSON, copy from source if needed, etc...)
      */
     Config() {
-        String configFileName = "config.json";
-        File configFile = new File("data", configFileName);
+
         URL urlSourceJson = ClassLoader.getSystemResource(configFileName);
 
         try {
@@ -40,13 +43,17 @@ public enum Config {
                 Files.copy(urlSourceJson.openStream(), configFile.toPath());
             }
 
-
-            defaultJson = gson.fromJson(new String(urlSourceJson.openStream().readAllBytes()), JsonObject.class);
+            InputStream stream = urlSourceJson.openStream();
+            defaultJson = gson.fromJson(new String(stream.readAllBytes()), JsonObject.class);
 
             BufferedReader reader = new BufferedReader(new FileReader(configFile.toString()));
             rootJson = gson.fromJson(reader, JsonObject.class);
             reader.close();
+            stream.close();
         } catch(IOException e) {
+            logger.error("Config threw while loading a {}: {}",
+                    e.getClass().getSimpleName(), e.getMessage());
+            Sentry.captureException(e);
             e.printStackTrace();
         }
     }
@@ -95,7 +102,7 @@ public enum Config {
      * Save the rootJson to the root config.json
      */
     public void saveModifications() throws IOException {
-        FileWriter writer = new FileWriter("config.json");
+        FileWriter writer = new FileWriter(configFile);
         writer.write(gson.toJson(rootJson));
         writer.flush();
         writer.close();
